31168672de72c5a2e706c53ca23f6664fb8a3b20
Matthew <morfeam1@hawkmail.newpaltz.edu>
2020-10-12 14:51:27 -0400

    In deep trouble...

--- 2020Fall2/client/src/views/Home.vue
+++ 2020Fall2/client/src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Welcome</h2>
   </div>
 </template>
 
@@ -12,7 +13,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
