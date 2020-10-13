b0a0a7b8a237566566829a21b76c936399197c3f
Matthew <morfeam1@hawkmail.newpaltz.edu>
2020-10-13 17:10:36 -0400

    Catchup for Feed elements

---
+++ 2020Fall3/client/src/views/Feed.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class = 'page'>
+      <h2 class = 'title is-2'>Feed Page</h2>
+
+        <div class="columns">
+            <div class = "column is-two-thirds">
+                <Post />  
+            </div>
+            <div class = "column is-one-third">
+                <SideBar />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Post from "@/components/Post";
+import SideBar from "@/components/SideBar";
+export default {
+    components: {
+        Post, SideBar
+    }
+}
+</script>
+
+<style>
+    .card{
+        margin-top: 15px; 
+    }
+</style>
