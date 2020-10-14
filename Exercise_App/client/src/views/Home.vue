1a4194a12132d54ce759ce1dc70ea02e40127177
Matthew <morfeam1@hawkmail.newpaltz.edu>
2020-10-13 23:09:06 -0400

    Further App Creations

---
+++ Exercise_App/client/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <div class="columns">
+      <div class="column is-half">
+      First column
+      </div>
+      <div class="column is-half">
+      Second column
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+   
+  }
+}
+</script>
