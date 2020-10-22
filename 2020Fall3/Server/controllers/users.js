762fdc6fe10cf3ed061dc89e66ba3970fc47b755
Matthew <morfeam1@hawkmail.newpaltz.edu>
2020-10-22 15:14:02 -0400

    Table code

--- 2020Fall3/Server/controllers/users.js
+++ 2020Fall3/Server/controllers/users.js
@@ -22,3 +22,39 @@ router.get('/', async (req, res) => {
   })
   
 module.exports = router;
+//mysql 2 table creation code
+/* 
+CREATE TABLE IF NOT EXISTS `Users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `created_at` DATETIME NOT NULL,
+  `update_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `FirstName` VARCHAR(45) NOT NULL,
+  `LastName` VARCHAR(45) NOT NULL,
+  `DOB` DATETIME NULL,
+  `Password` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `ContactMethods`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ContactMethods` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `created_at` DATETIME NOT NULL,
+  `update_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `Type` VARCHAR(45) NOT NULL,
+  `Value` VARCHAR(45) NOT NULL,
+  `IsPrimary` BIT NOT NULL DEFAULT 0,
+  `CanSpam` BIT NOT NULL DEFAULT 0,
+  `User_id` INT NOT NULL,
+  `Users_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_ContactMethods_Users_idx` (`Users_id` ASC) ,
+  CONSTRAINT `fk_ContactMethods_Users`
+    FOREIGN KEY (`Users_id`)
+    REFERENCES `Users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+*/
