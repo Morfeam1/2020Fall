74be677813e821f7d16e4ab8b0df507d161cd189
Matthew <morfeam1@hawkmail.newpaltz.edu>
2020-10-29 15:15:19 -0400

    URL params and Parameterized queries

--- 2020Fall3/Server/controllers/users.js
+++ 2020Fall3/Server/controllers/users.js
@@ -7,18 +7,29 @@ const users = require('../models/users');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    //throw { status: 501, message: "This is a fake error"}
-    res.send(
-        users.getAll()
-    );
-    console.log("Finished get users");
+router
+  .get('/',(req,res,next) => {
+    users.getAll().then(x=>res.send(x))
+    .catch(next);
   })
-  .post('/', (req, res) => {
-      const newUser = users.add(
-          req.query.name,
-          req.query.age);
-          res.send(newUser);
+  .get('/id',(req,res,next) => {
+    const id = +req.params.id;
+    if(!id) return next();
+    users.get(id).then(x=>res.send(x))
+    .catch(next);
+  })
+  .get('/types',(req,res,next) => {
+    users.getTypes().then(x=>res.send(x))
+    .catch(next);
+  })
+  .get('/search',(req,res,next) => {
+    users.search(req.query.q).then(x=>res.send(x))
+    .catch(next);
+  })
+  .post('/', (req, res,next) => {
+      users.add(req.query.name,req.query.age).then(newUser => {
+        res.send(newUser);
+      }).catch(next)
   })
   
 module.exports = router;
--- 2020Fall3/Server/models/users.js
+++ 2020Fall3/Server/models/users.js
@@ -3,19 +3,28 @@
 
 const mysql = require('./mysql');
 
-const data = [{name: 'Moshe', age: 43},
-{name: 'Biden', age: 78}]
+//const data = [{name: 'Moshe', age: 43},
+//{name: 'Biden', age: 78}]
 
  async function getAll(){
     //await Promise.resolve()
     //throw { status: 501, message: "This is a fake error"}
     console.log("Called Get All")
-    return mysql.query(`SELECT = FROM Users`);
+    return await mysql.query(`SELECT = FROM Users`);
+}
+
+async function get(id){
+    return await mysql.query(`SELECT = FROM Users WHERE id=?`, [id]);
+}
+
+async function getTypes(){
+    return await mysql.query(`SELECT id, Name FROM Types WHERE Type_id - 2`);
 }
 function add(name,age){
     data.push({name,age});
 }
 
+const search = async q => await mysql.query(`SELECT id, FirstName, LastName, FROM Users WHERE LastName LIKE ? OR FirstName LIKE ?; `, [`%${q}%`, `%${q}%`]);
 
-module.exports = {rand, getAll,add, search: q => data.filter(x=> x.name == q)}
+module.exports = {rand, getAll,get,add,getTypes,search}
 
